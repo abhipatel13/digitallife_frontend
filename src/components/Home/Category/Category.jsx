84b2d6254e8b70bce13fb27f5997ddeb1b4cdf15
abhipatel1369 <72403818+abhipatel1369@users.noreply.github.com>
2023-08-03 11:16:18 +0530

    image-preview-fix

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
 
 const Category = ({categories}) => {
   const navigate = useNavigate();
+  console.log("Category",categories?.data?.data[0]?.attributes.img.data.attributes.url);
     return (
         <div className="shop-by-category">
           <div className="categories">
           {
             categories?.data && categories.data.data.map((item)=>(
             <div key={item.id} className="category" onClick={() => navigate(`/category/${item.id}`)}>
-                <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt=""/>
+                <img src={item?.attributes?.img?.data?.attributes?.url} alt=""/>
             </div>
             ))
           }
