b1705f7a1e9cf2d61ed84e36a2394d89fac0dcce
abhipatel1369 <72403818+abhipatel1369@users.noreply.github.com>
2023-08-03 14:19:16 +0530

    'image-visible-color-change'

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -55,7 +55,7 @@ const Search = ({ setShowSearch }) => {
                             <div className="image-container">
                                 <img
                                     src={
-                                        process.env.REACT_APP_DEV_URL + item.attributes.img.data[0].attributes.url
+                                        item.attributes.img.data[0].attributes.url
                                     }
                                 />
                             </div>
